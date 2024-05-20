26e087eee0c9807af30187e5c8fc6b9b906c3932
Durlav Parajuli <durlove60@gmail.com>
2024-05-21 00:56:30 +0545

    production changes

--- customerSide/config.php
+++ customerSide/config.php
@@ -1,8 +1,8 @@
 <?php // Rememeber to change the username,password and database name to acutal values
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'restaurantDB');
+define('DB_USER', 'necceduc_pos');
+define('DB_PASS', '$+8,a[dH1]k1');
+define('DB_NAME', 'necceduc_poss');
 
 //Create Connection
 $link = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
--- expense/includes/dbconnection.php
+++ expense/includes/dbconnection.php
@@ -1,5 +1,11 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "restaurantdb");
+$con = mysqli_connect("localhost", "necceduc_pos", "$+8,a[dH1]k1", "necceduc_poss");
 if (mysqli_connect_errno()) {
   echo "Connection Fail" . mysqli_connect_error();
 }
+
+
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'necceduc_pos'); 
+// define('DB_PASS', '$+8,a[dH1]k1');
+// define('DB_NAME', 'necceduc_poss');
